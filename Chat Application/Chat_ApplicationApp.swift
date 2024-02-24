b7aeaabb6711c1a76302b42a19b50f059fd89850
Darahaas N <65232365+Darahaas2001@users.noreply.github.com>
2024-02-24 11:49:33 -0600

    Initial Commit

---
+++ Chat Application/Chat_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_ApplicationApp.swift
+//  Chat Application
+//
+//  Created by Darahaas Nallagatla on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Chat_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
